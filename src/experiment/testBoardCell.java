b1ffa62aa126427b218a2bd3fa4dae1d57de9268
OJackson <oliviajackson@mymail.mines.edu>
2021-02-25 17:52:54 -0700

    renamed board cell file to be all caps

--- src/experiment/testBoardCell.java
+++ src/experiment/testBoardCell.java
@@ -1,9 +1,16 @@
 package experiment;
 
-public class testBoardCell {
+public class TestBoardCell {
 	private int row;
 	private int column;
+	public TestBoardCell(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
 	
-	
+	void addAdjacency(TestBoardCell cell) {
+		
+	}
 	
 }
