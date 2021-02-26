e1b19405722e3ae5df6855ddc8a5a07c4046fa82
OJackson <oliviajackson@mymail.mines.edu>
2021-02-25 18:43:56 -0700

    Lost some commits due to a error using git. Renamed test board cell file and added variables

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -7,6 +7,9 @@ public class TestBoardCell {
 	private int row;
 	private int column;
 	private Set<TestBoardCell> adjacencyList; //List of adjacent tiles
+	private boolean occupied;
+	private boolean isRoom;
+	
 	
 	//COnstructor sets row and col values
 	public TestBoardCell(int row, int column) {
@@ -22,27 +25,27 @@ public class TestBoardCell {
 	
 	//Get adjacent list
 	public Set<TestBoardCell> getAdjList() {
-		
+		return adjacencyList;
 	}
 	
 	//set what room the tile is in
 	void setRoom(boolean check) {
-		
+		isRoom=check;
 	}
 	
 	//Check if the tile is in the room
 	boolean isRoom() {
-		
+		return isRoom;
 	}
 	
 	//Set if the tile is already occupied 
 	void setOccupied(boolean check) {
-		
+		occupied= check;
 	}
 	
 	//check if the room is occupied
 	boolean getOccupied() {
-		
+		return occupied;
 	}
 	
 }
