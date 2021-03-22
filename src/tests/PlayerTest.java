71e5b47f732ca14dbd4a21ce7b708f01130bd906
eebag <gabehohman@hotmail.com>
2021-03-21 20:11:40 -0600

    Added testing class for all things related to implementing the Player class

---
+++ src/tests/PlayerTest.java
@@ -0,0 +1,15 @@
+package tests;
+import static org.junit.Assert.*;
+
+/**
+ * Tests for all things related to the implementation of the Player class
+ * 
+ * @author Gabe Hohman
+ * @author Olivia Jackson
+ *
+ */
+public class PlayerTest {
+	
+	
+	
+}
