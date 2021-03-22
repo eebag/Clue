a926acb16dd4fd31800f352923cdd2ea7c79296b
OJackson <oliviajackson@mymail.mines.edu>
2021-03-21 20:40:58 -0600

    Setup test file

--- src/tests/PlayerTest.java
+++ src/tests/PlayerTest.java
@@ -1,6 +1,11 @@
 package tests;
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+import org.junit.jupiter.api.*;
+
+import clueGame.Board;
+
 /**
  * Tests for all things related to the implementation of the Player class
  * 
@@ -9,7 +14,24 @@ import static org.junit.Assert.*;
  *
  */
 public class PlayerTest {
-	
+	// We make the Board static because we can load it one time and 
+		// then do all the tests. 
+		private static Board board;
+
+		@BeforeAll
+		public static void setUp() {
+			// Board is singleton, get the only instance
+			board = Board.getInstance();
+			// set the file names to use my config files
+			board.setConfigFiles("MapOfCampusCLUE.csv", "ClueSetup.txt");		
+			// Initialize will load config files 
+			board.initialize();
+		}
+		
+		@Test
+		public void peopleLoaded() {
+			
+		}
 	
 	
 }
