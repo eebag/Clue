165186bc82636fad22ba933a4866555ed6e43445
OJackson <oliviajackson@mymail.mines.edu>
2021-04-04 20:08:38 -0600

    Added lower pannel

--- src/clueGame/GameControlPanel.java
+++ src/clueGame/GameControlPanel.java
@@ -1,8 +1,10 @@
 package clueGame;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 
 public class GameControlPanel extends JPanel{
 	//Size of the control GUI
@@ -16,7 +18,7 @@ public class GameControlPanel extends JPanel{
 	
 	public GameControlPanel() {
 		JPanel upperPanel = new JPanel(); // pannel that contains player, roll, and accusation controls
-		JPanel guessPanel = new JPanel(); // pannel that contains guess and guess results
+		JPanel lowerPanel = new JPanel(); // pannel that contains guess and guess results
 		
 		//UI for upperPanel
 		JPanel turnInformation = new JPanel(); // panel to hold information about whose turn it is
@@ -28,12 +30,32 @@ public class GameControlPanel extends JPanel{
 		
 		upperPanel.add(turnInformation, BorderLayout.WEST);
 		
+		
+		//Create boarder
+		Border blackline = BorderFactory.createLineBorder(Color.black);
+		
+		//lower pannel setup
+		//GUess setup
+		JPanel guessPanelBorder= new JPanel(); //Displays guess and guess name
+		guessPanelBorder.setBorder(blackline); //Give it a boarder
+		guessPanelBorder.setName("Guess"); //Name it guess
+		
+		//Guess result setup
+		JPanel guessResultPanelBorder= new JPanel(); //Displays guess result and the result value
+		guessResultPanelBorder.setBorder(blackline); //Give it a boarder
+		guessResultPanelBorder.setName("Guess Result"); //name it guess result
+		
+		//Add text fields to both
+		guess= new JTextField();
+		guessResult= new JTextField();
+		
+		guessPanelBorder.add(guess, BorderLayout.WEST);
+		guessResultPanelBorder.add(guessResult, BorderLayout.EAST);
+		
+		
 		add(upperPanel,BorderLayout.NORTH);
-		add(guessPanel, BorderLayout.SOUTH);
-		
-		
-		
-		
+		add(lowerPanel, BorderLayout.SOUTH);
+
 	}
 	
 	public static void main(String[] args) {
