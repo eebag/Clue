db4a20d12770a9227a7f1370c4d464a224c63f5e
eebag <gabehohman@hotmail.com>
2021-04-04 19:32:06 -0600

    Worked on GameControlPanel

--- src/clueGame/GameControlPanel.java
+++ src/clueGame/GameControlPanel.java
@@ -4,6 +4,10 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class GameControlPanel extends JPanel{
+	//Size of the control GUI
+	public final static int SIZE_X = 750;
+	public final static int SIZE_Y = 180;
+	
 	
 	public GameControlPanel() {
 		
@@ -11,9 +15,9 @@ public class GameControlPanel extends JPanel{
 	
 	public static void main(String[] args) {
 		GameControlPanel panel = new GameControlPanel();  // create the panel
-		JFrame frame = new JFrame();  // create the frame 
+		JFrame frame = new JFrame();  // create the frame
 		frame.setContentPane(panel); // put the panel in the frame
-		frame.setSize(750, 180);  // size the frame
+		frame.setSize(SIZE_X, SIZE_Y);  // size the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
 		frame.setVisible(true); // make it visible
 		
